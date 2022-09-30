8ce5a87e52be29194e7cfcbfa64588edb87edaff
Duruwilly <duruprincewilluzochukwu@gmail.com>
2022-09-30 22:46:36 +0100

    final commit

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,7 +7,7 @@ const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
-    return <h1>Loading...</h1>
+    return <Spinner description='loading...' />;
   }
 
   return loggedIn ? <Outlet /> : <Navigate to="/login" />;
