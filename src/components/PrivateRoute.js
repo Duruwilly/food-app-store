0d878629a244b5d09dd9b1d81897d6120fef9688
Duruwilly <duruprincewilluzochukwu@gmail.com>
2022-10-01 07:54:59 +0100

    hello

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
-import { Spinner } from "./Spinner";
+
 
 
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
-    return <Spinner description='loading...' />;
+    return <h1>loading...</h1>
   }
 
   return loggedIn ? <Outlet /> : <Navigate to="/login" />;
